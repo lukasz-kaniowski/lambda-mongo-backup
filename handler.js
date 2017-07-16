0e43dcc1e6df27e7fdbc59cd6c7ee8c2f7fa7ca1
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2017-07-16 11:35:36 +0100

    Add lambda bucket permission

--- handler.js
+++ handler.js
@@ -1,7 +1,7 @@
 'use strict';
 const backupService = require('mongo3-s3-streaming-backup');
 
-module.exports.hello = (event, context, done) => {
+module.exports.backup = (event, context, done) => {
 
   backupService.backup({
     mongoUrl: process.env.mongoUrl,
